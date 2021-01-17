c5dbe7b43e2dece1c47a68fc6d6fb48120e12cda
Juan Pablo Arbelaez <juanpablo-arbelaez@hotmail.com>
2021-01-17 15:06:14 +0700

    formatting style

--- main.py
+++ main.py
@@ -23,33 +23,31 @@ def filename_from_url(url: str) -> str:
 if __name__ == "__main__":
     url = args.get("url")
     file_format = args.get("format")
+    
     output_dir = f'{os.getcwd()}/{args.get("output")}'
     if not os.path.exists(output_dir):
         print(f"[INFO] creating {output_dir} directory.")
         os.makedirs(output_dir)
 
-
     scrap = args.get("scrap")
-
     if scrap == "category":
         if not url:
             # Example url if -u not proviced in cmd option
             url = "https://www.lazada.vn/bikes/" 
         
         filename = filename_from_url(url)  
-        
         print(f"[INFO] Creating Scraper for: *{scrap}* in {url}")
         cs = CategoryScraper(output_dir, file_format)
         cs.scrap_and_save(filename, url)
 
     elif scrap == "product":
+        file_format = 'json' # should always be json, as csv is not suitable for the product         
+
         if not url:
             # Example url if -u not proviced in cmd option
             url = "https://www.lazada.sg/products/arduino-starter-kit-official-english-i476386619-s1297160688.html"
         
         filename = filename_from_url(url)  
-        file_format = 'json' # should always be json, as csv is not suitable for the product         
-        
         print(f"[INFO] Creating Scraper for: *{scrap}* in {url}")
         ps = ProductScraper(output_dir, file_format)
         ps.scrap_and_save(filename, url)
